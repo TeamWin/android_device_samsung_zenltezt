334b425ba351eebeb0daedf3a23ed886a4f6c206
James Christopher Adduono <jc@adduono.com>
2016-04-05 22:28:55 -0400

    Initialize device tree for zenltezt

---
+++ omni_zenltezt.mk
@@ -0,0 +1,14 @@
+# Release name
+PRODUCT_RELEASE_NAME := zenltezt
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zenltezt
+PRODUCT_NAME := omni_zenltezt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G9287
+PRODUCT_MANUFACTURER := samsung
